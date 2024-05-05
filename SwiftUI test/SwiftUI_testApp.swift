37d4dfe297e755bab1ebfe440f0d4e1b6bab0638
Altair741 <104821082+Altair741@users.noreply.github.com>
2024-05-05 20:29:32 +0930

    Initial Commit

---
+++ SwiftUI test/SwiftUI_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_testApp.swift
+//  SwiftUI test
+//
+//  Created by Ethan Li on 5/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
