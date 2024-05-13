adfe103fc96720cf62e0812f589eb46f28b02268
MinguKang047 <112845676+MinguKang047@users.noreply.github.com>
2024-05-14 05:17:43 +1000

    Update MyModal.swift to connect with Monofilament test

--- SwiftUI test/MyModal.swift
+++ SwiftUI test/MyModal.swift
@@ -15,33 +15,41 @@ struct MyModal: View {
             Spacer()
             
             // Show the modal
-            .fullScreenCover(isPresented: $showModal) {
-                NavigationView {
-                    ZStack {
-                        Color.yellow.ignoresSafeArea()
-                        VStack {
-                            Image(systemName: "exclamationmark.triangle")
-                            .foregroundColor(.red)
-                            .font(.system(size: 100))
-                            .padding(.top)
-                            
-                            Text("Alert!!")
-                                .padding(.top, 30)
-                                .padding(.bottom, 20)
-                            
-                            
-                            NavigationLink(destination: Q3()) {
-                                Text("Next")
-                                    .padding()
-                                    .frame(width: 120, height: 50)
-                                    .background(Color.green)
-                                    .foregroundColor(.white)
-                                    .cornerRadius(25)
+                .fullScreenCover(isPresented: $showModal) {
+                    NavigationView {
+                        ZStack {
+                            Color.yellow.ignoresSafeArea()
+                            VStack {
+                                Image(systemName: "exclamationmark.triangle")
+                                    .foregroundColor(.red)
+                                    .font(.system(size: 100))
+                                    .padding(.top)
+                                
+                                Text("This test requires a monofilament, if you do not have one press skip and move onto the Ipswich Touch Test")
+                                    .padding(.top, 30)
+                                    .padding(.bottom, 20)
+                                
+                                NavigationLink(destination: Monofilament_test()) {
+                                    Text("PROCEED")
+                                        .padding()
+                                        .frame(width: 120, height: 50)
+                                        .background(Color.green)
+                                        .foregroundColor(.white)
+                                        .cornerRadius(25)
+                                    
+                                    NavigationLink(destination: EndPage()) {
+                                        Text("SKIP")
+                                            .padding()
+                                            .frame(width: 120, height: 50)
+                                            .background(Color.green)
+                                            .foregroundColor(.white)
+                                            .cornerRadius(25)
+                                    }
+                                }
                             }
                         }
                     }
                 }
-            }
         }
     }
 }
