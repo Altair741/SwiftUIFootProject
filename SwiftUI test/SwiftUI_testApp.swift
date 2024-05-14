84c71bdc19e23254f68454973066a785fc4d8ca7
kanmy047 <mg.kang0518@gmail.com>
2024-05-15 06:07:54 +0930

    injects instances of global variables to enable global usage throughout the app

--- SwiftUI test/SwiftUI_testApp.swift
+++ SwiftUI test/SwiftUI_testApp.swift
@@ -7,13 +7,16 @@
 
 import SwiftUI
 
-
-
 @main
 struct SwiftUI_testApp: App {
+    @StateObject var userAnswer = UserAnswer()
+    @StateObject var locationManager = LocationManager2()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(userAnswer)
+                .environmentObject(locationManager)
         }
     }
 }
