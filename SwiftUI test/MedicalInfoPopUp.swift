473612bcad8f0c938b7d6f2168ef836ba686ffba
kanmy047 <mg.kang0518@gmail.com>
2024-05-15 06:10:48 +0930

    Hide defalut navigationBackButton

--- SwiftUI test/MedicalInfoPopUp.swift
+++ SwiftUI test/MedicalInfoPopUp.swift
@@ -19,46 +19,51 @@ struct MedicalInfoPopUp: View {
             {
                 Text(medicalInfoString)
                 // Add conditional logic to navigate different view
-                if task == "ContentView"{
-                    NavigationLink(destination: ContentView()) {
+                if task == "Q1"{
+                    NavigationLink(destination: Q1().navigationBarBackButtonHidden(true)) {
                         Text("Back")
                             .frame(width: 120, height: 50)
                             .background(Color.green)
                             .foregroundColor(.white)
                             .cornerRadius(25)
                     }.simultaneousGesture(TapGesture())
+
+                    
                 }else if task == "Q2"
                 {
-                    NavigationLink(destination: Q2()) {
+                    NavigationLink(destination: Q2().navigationBarBackButtonHidden(true)) {
                         Text("Back")
                             .frame(width: 120, height: 50)
                             .background(Color.green)
                             .foregroundColor(.white)
                             .cornerRadius(25)
                     }.simultaneousGesture(TapGesture())
+
                 }else if task == "Q3"
                 {
-                    NavigationLink(destination: Q3()) {
+                    NavigationLink(destination: Q3().navigationBarBackButtonHidden(true)) {
                         Text("Back")
                             .frame(width: 120, height: 50)
                             .background(Color.green)
                             .foregroundColor(.white)
                             .cornerRadius(25)
                     }.simultaneousGesture(TapGesture())
+
                     
                 }else if task == "Q4"
                 {
-                    NavigationLink(destination: Q4()) {
+                    NavigationLink(destination: Q4().navigationBarBackButtonHidden(true)) {
                         Text("Back")
                             .frame(width: 120, height: 50)
                             .background(Color.green)
                             .foregroundColor(.white)
                             .cornerRadius(25)
                     }.simultaneousGesture(TapGesture())
+
                     
                 }else if task == "Q5"
                 {
-                    NavigationLink(destination: Q5()) {
+                    NavigationLink(destination: Q5().navigationBarBackButtonHidden(true)) {
                         Text("Back")
                             .frame(width: 120, height: 50)
                             .background(Color.green)
@@ -66,7 +71,7 @@ struct MedicalInfoPopUp: View {
                             .cornerRadius(25)
                     }.simultaneousGesture(TapGesture())
                 }else if task == "Q6"
-                {NavigationLink(destination: Q6()) {
+                {NavigationLink(destination: Q6().navigationBarBackButtonHidden(true)) {
                     Text("Back")
                         .frame(width: 120, height: 50)
                         .background(Color.green)
@@ -75,7 +80,7 @@ struct MedicalInfoPopUp: View {
                 }.simultaneousGesture(TapGesture())
                     
                 }else if task == "Q7"
-                {NavigationLink(destination: Q7()) {
+                {NavigationLink(destination: Q7().navigationBarBackButtonHidden(true)) {
                     Text("Back")
                         .frame(width: 120, height: 50)
                         .background(Color.green)
@@ -84,7 +89,7 @@ struct MedicalInfoPopUp: View {
                 }.simultaneousGesture(TapGesture())
                     
                 }else if task == "Q8"
-                {NavigationLink(destination: Q8()) {
+                {NavigationLink(destination: Q8().navigationBarBackButtonHidden(true)) {
                     Text("Back")
                         .frame(width: 120, height: 50)
                         .background(Color.green)
@@ -93,7 +98,7 @@ struct MedicalInfoPopUp: View {
                 }.simultaneousGesture(TapGesture())
                     
                 }else if task == "Q9"
-                {NavigationLink(destination: Q9()) {
+                {NavigationLink(destination: Q9().navigationBarBackButtonHidden(true)) {
                     Text("Back")
                         .frame(width: 120, height: 50)
                         .background(Color.green)
@@ -102,7 +107,7 @@ struct MedicalInfoPopUp: View {
                 }.simultaneousGesture(TapGesture())
                 }else if task == "Q10"
                 {
-                    NavigationLink(destination: Q10()) {
+                    NavigationLink(destination: Q10().navigationBarBackButtonHidden(true)) {
                         Text("Back")
                             .frame(width: 120, height: 50)
                             .background(Color.green)
@@ -112,7 +117,7 @@ struct MedicalInfoPopUp: View {
                 }
                 else if task == "Q11"
                 {
-                    NavigationLink(destination: Q11()) {
+                    NavigationLink(destination: Q11().navigationBarBackButtonHidden(true)) {
                         Text("Back")
                             .frame(width: 120, height: 50)
                             .background(Color.green)
@@ -122,7 +127,7 @@ struct MedicalInfoPopUp: View {
                     
                 }else if task == "Q12"
                 {
-                    NavigationLink(destination: Q12()) {
+                    NavigationLink(destination: Q12().navigationBarBackButtonHidden(true)) {
                         Text("Back")
                             .frame(width: 120, height: 50)
                             .background(Color.green)
@@ -132,36 +137,42 @@ struct MedicalInfoPopUp: View {
                 }
                 else if task == "DP_test"
                 {
-                    NavigationLink(destination: DP_test()) {
+                    NavigationLink(destination: DP_test().navigationBarBackButtonHidden(true)) {
                         Text("Back")
                             .frame(width: 120, height: 50)
                             .background(Color.green)
                             .foregroundColor(.white)
                             .cornerRadius(25)
                     }.simultaneousGesture(TapGesture())
+
                 }
                 else if task == "PT_test"
                 {
-                    NavigationLink(destination: PT_test()) {
+                    NavigationLink(destination: PT_test().navigationBarBackButtonHidden(true)) {
                         Text("Back")
                             .frame(width: 120, height: 50)
                             .background(Color.green)
                             .foregroundColor(.white)
                             .cornerRadius(25)
                     }.simultaneousGesture(TapGesture())
+
                 }
                 else if task == "Monofilament_test"
                 {
-                    NavigationLink(destination: Monofilament_test()) {
+                    NavigationLink(destination: Monofilament_test().navigationBarBackButtonHidden(true)) {
                         Text("Back")
                             .frame(width: 120, height: 50)
                             .background(Color.green)
                             .foregroundColor(.white)
                             .cornerRadius(25)
-                    }.simultaneousGesture(TapGesture())
+                    }
+                    .simultaneousGesture(TapGesture())
+                    
+
                 }
                 
             }
+
         }
     }
 }
