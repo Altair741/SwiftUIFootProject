fd67432068236513e53b62fdf158e51ac2823b6f
kanmy047 <mg.kang0518@gmail.com>
2024-05-17 13:59:50 +0930

    Update WithPictorial.swift
    change video form from Youtube to Local video

--- SwiftUI test/WithPictorial.swift
+++ SwiftUI test/WithPictorial.swift
@@ -5,35 +5,18 @@
 //  Created by 강민구 on 3/4/2024.
 //
 
+
 import SwiftUI
-import WebKit
+import AVKit
 
 struct WithPictorial: View {
-    let ID: String
-    
+    let videoName: String
+
     var body: some View {
-        
-        Video(videoID: ID)
-            .frame(width:350, height: 190)
+        VideoPlayer(player: AVPlayer(url: Bundle.main.url(forResource: videoName, withExtension: "mp4")!))
+            .frame(width: 350, height: 190)
             .cornerRadius(12)
             .padding(.horizontal, 24)
     }
 }
 
-struct Video: UIViewRepresentable{
-    let videoID : String
-    
-    func makeUIView(context: Context) -> some WKWebView {
-            return WKWebView()
-        
-    }
-    func updateUIView(_ uiView: UIViewType, context: Context) {
-        guard let YouTubeURL = URL(string:
-                                    "https://www.youtube.com/watch?v=\(videoID)")
-        else
-        {return}
-        uiView.scrollView.isScrollEnabled = false
-        uiView.load(URLRequest(url: YouTubeURL))
-    }
-    
-}
