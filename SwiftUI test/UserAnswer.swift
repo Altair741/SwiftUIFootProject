8e93b0919459f2213ad96350d54932bf8606200a
kanmy047 <112845676+MinguKang047@users.noreply.github.com>
2024-05-22 12:54:01 +0930

    Add more properties

--- SwiftUI test/UserAnswer.swift
+++ SwiftUI test/UserAnswer.swift
@@ -10,16 +10,21 @@ import Combine
 
 class UserAnswer : ObservableObject
 {
-    @Published var userDetails: [String] = Array(repeating: "Not given", count: 4)
+//    @Published var userDetails: [String] = Array(repeating: "Not given", count: 4)
     @Published var answerRecord: [String] = Array(repeating: "Not Answered", count: 20)
     @Published var assessmentRecord : [String] = Array(repeating: "Not Answered", count: 12)
     @Published var dateRecords: [Date] = Array(repeating: Date(), count: 2)
-    @Published var riskFactor: [String] = []
+    @Published var riskFactor: [String] = Array(repeating: "Fuck", count : 25)
+    
     // 0 ~ 5 : Monofilament test, 6 ~ 11 : IPSWICH test
     // The score go up when the paitient say No
     @Published var MonofilamentScore : Int = 0
     @Published var IPSWICHScore : Int = 0
+    @Published var system_g_risk = ""
+    @Published var user_s_risk = ""
+    @Published var pdf_url : URL?
     
+
     @Published var userInfo : [String] = ["", "", ""]
 
 }
