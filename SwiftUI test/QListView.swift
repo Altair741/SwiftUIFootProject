6c0dcd1cee3b1859a70642f4b33c27418b75f3a4
MinguKang047 <112845676+MinguKang047@users.noreply.github.com>
2024-06-02 09:34:29 +0930

    Add input validation for touch tests

--- SwiftUI test/QListView.swift
+++ SwiftUI test/QListView.swift
@@ -1,20 +1,24 @@
-
 import SwiftUI
 
 struct QuestionItem: Identifiable {
     let id = UUID()
     let task: String
-    let index: Int
     let view: AnyView
 }
 
 struct QListView: View {
+    @State private var assessmentTaskDone = false
     @State private var isQuestionListExpanded = false
     @State private var currentViewOrder = 0
     @EnvironmentObject var answer : UserAnswer
     @State private var showAlert = false
-    @State private var isAnswered = false
-    
+    @State private var isAnsweredDP = false
+    @State private var isAnsweredPT = false
+    @State private var isAnsweredMono = false
+    @State private var isAnsweredSwitch = false
+
+
+
     let taskView: [AnyView] = [
         AnyView(Q1()),
         AnyView(Q2()),
@@ -32,80 +36,105 @@ struct QListView: View {
         AnyView(PT_test()),
         AnyView(MyModal()),
         AnyView(Monofilament_test()),
-        AnyView(IPSWICH_test())
+        AnyView(IPSWICH_test()),
+        AnyView(RiskCalculator()),
+        AnyView(SignInView())
     ]
     
     let taskList: [QuestionItem] = [
-        QuestionItem(task: "Callus", index:0,view: AnyView(Q1())),
-        QuestionItem(task: "Corns", index:1,view: AnyView(Q2())),
-        QuestionItem(task: "Tinea", index:2,view: AnyView(Q3())),
-        QuestionItem(task: "Ulcer", index:3,view: AnyView(Q4())),
-        QuestionItem(task: "Thickened toenails", index:4,view: AnyView(Q5())),
-        QuestionItem(task: "Ingrown toenails", index:5,view: AnyView(Q6())),
-        QuestionItem(task: "Bunions", index:6,view: AnyView(Q7())),
-        QuestionItem(task: "Clawed toes", index:7,view: AnyView(Q8())),
-        QuestionItem(task: "Flat feet", index:8,view: AnyView(Q9())),
-        QuestionItem(task: "High arched feet", index:9,view: AnyView(Q10())),
-        QuestionItem(task: "Charcot foot",index:10, view: AnyView(Q11())),
-        QuestionItem(task: "Amputation", index:11,view: AnyView(Q12())),
-        QuestionItem(task: "DP test", index:12,view: AnyView(DP_test())),
-        QuestionItem(task: "PT test", index:13,view: AnyView(PT_test())),
-        QuestionItem(task: "MyModal", index:14,view: AnyView(MyModal())),
-        QuestionItem(task: "Monofilament Test", index:15,view: AnyView(Monofilament_test())),
-        QuestionItem(task: "IPSWICH touch test",index:16, view: AnyView(IPSWICH_test()))
+        QuestionItem(task: "Callus", view: AnyView(Q1())),
+        QuestionItem(task: "Corns", view: AnyView(Q2())),
+        QuestionItem(task: "Tinea", view: AnyView(Q3())),
+        QuestionItem(task: "Ulcer", view: AnyView(Q4())),
+        QuestionItem(task: "Thickened toenails", view: AnyView(Q5())),
+        QuestionItem(task: "Ingrown toenails", view: AnyView(Q6())),
+        QuestionItem(task: "Bunions", view: AnyView(Q7())),
+        QuestionItem(task: "Clawed toes", view: AnyView(Q8())),
+        QuestionItem(task: "Flat feet", view: AnyView(Q9())),
+        QuestionItem(task: "High arched feet", view: AnyView(Q10())),
+        QuestionItem(task: "Charcot foot", view: AnyView(Q11())),
+        QuestionItem(task: "Amputation", view: AnyView(Q12())),
+        QuestionItem(task: "DP test", view: AnyView(DP_test())),
+        QuestionItem(task: "PT test", view: AnyView(PT_test())),
+        QuestionItem(task: "MyModal", view: AnyView(MyModal())),
+        QuestionItem(task: "Monofilament test", view: AnyView(Monofilament_test())),
+        QuestionItem(task: "IPSWICH touch test", view: AnyView(IPSWICH_test())),
+        QuestionItem(task: "Risk", view: AnyView(RiskCalculator())),
+        QuestionItem(task: "SignInView", view: AnyView(SignInView()))
     ]
     
     var body: some View {
+        
         NavigationView {
+            
             VStack {
-                // Display the current question view based on taskList
-                taskList[currentViewOrder].view
+                taskView[currentViewOrder]
                 
-                // Navigation Buttons (Modified)
                 HStack {
                     Button(action: {
                         withAnimation {
-                            // Find the index of the previous question in taskList using the id property
-                            if let prevIndex = taskList.firstIndex(where: { $0.id == taskList[currentViewOrder].id }), prevIndex > 0 {
-                                currentViewOrder = prevIndex - 1
-                            } else {
-                                currentViewOrder = taskList.count - 1
+                            
+                            currentViewOrder -= 1
+                            if currentViewOrder < 0 {
+                                currentViewOrder = taskView.count - 1
                             }
+                            
+                            
                         }
                     }) {
                         Text("Back")
                             .padding()
                     }
-                    
                     Spacer()
                     
                     Button(action: {
                         withAnimation {
-                            if taskList[currentViewOrder].task == "PT test" && !isAnswered {
-                                checkQuestionCompletion_PT_test()
-                            } else if taskList[currentViewOrder].task == "DP test" && !isAnswered {
+                            
+                            
+    
+                            if taskList[currentViewOrder].task == "DP test" && !isAnsweredDP{
                                 checkQuestionCompletion_DP_test()
-                            } else if taskList[currentViewOrder].task == "MyModal" {
-                                if checkMedicalTool() {
-                                    // Find the index of the next question in taskList using id
-                                    if let nextIndex = taskList.firstIndex(where: { $0.id == taskList[currentViewOrder].id }), nextIndex < taskList.count - 1 {
-                                        currentViewOrder = nextIndex + 1
-                                    } else {
-                                        currentViewOrder = 0
-                                    }
-                                } else {
-                                    // Find the index of the next question after skipping in taskList using id
-                                    if let nextIndex = taskList.firstIndex(where: { $0.id == taskList[currentViewOrder].id }), nextIndex < taskList.count - 2 {
-                                        currentViewOrder = nextIndex + 2
-                                    } else {
+                            }
+                            else if taskList[currentViewOrder].task == "PT test" && !isAnsweredPT{
+                                checkQuestionCompletion_PT_test()
+
+                            }
+                            else if taskList[currentViewOrder].task == "Monofilament test" {
+                                checkQuestionCompletion_MonoTest()
+                                if(isAnsweredMono)
+                                {
+                                    currentViewOrder += 2
+
+                                }
+                                
+                            }
+                            
+                            else if taskList[currentViewOrder].task == "IPSWICH touch test" && !isAnsweredSwitch{
+                                checkQuestionCompletion_SWTCH()
+                            }
+                            
+                            else if taskList[currentViewOrder].task == "MyModal"
+                            {
+                                if(checkMedicalTool())
+                                {
+                                    currentViewOrder += 1
+                                    
+                                    if currentViewOrder >= taskView.count {
                                         currentViewOrder = 0
                                     }
                                 }
-                            } else {
-                                // Find the index of the next question in taskList using id
-                                if let nextIndex = taskList.firstIndex(where: { $0.id == taskList[currentViewOrder].id }), nextIndex < taskList.count - 1 {
-                                    currentViewOrder = nextIndex + 1
-                                } else {
+                                else{
+                                    currentViewOrder += 2
+                                    
+                                    if currentViewOrder >= taskView.count {
+                                        currentViewOrder = 0
+                                    }
+                                }
+                            }
+                            else {
+                                currentViewOrder += 1
+                                
+                                if currentViewOrder >= taskView.count {
                                     currentViewOrder = 0
                                 }
                             }
@@ -114,98 +143,105 @@ struct QListView: View {
                         Text("Next")
                             .padding()
                     }
-                    .padding()
                     
-                    
-                    DisclosureGroup("Jump to Question", isExpanded: $isQuestionListExpanded) {
-                        List(taskList) { question in
-                            Button(action: {
-                                if let index = taskList.firstIndex(where: { $0.id == question.id }) {
-                                    if answer.answerRecord.count > index && answer.answerRecord[index] != "Not Answered" {
-                                        currentViewOrder = index
-                                        isQuestionListExpanded = false
-                                    } else if index <= currentViewOrder {  // Allow going back
-                                        currentViewOrder = index
-                                        isQuestionListExpanded = false
-                                    } else {
-                                        showAlert = true // Show alert if trying to skip ahead
-                                    }
-                                }
-                            }) {
-                                HStack {
-                                    Text("\(taskList.firstIndex(where: { $0.id == question.id })! + 1)")
-                                        .frame(minWidth: 20, alignment: .leading)
-                                        .foregroundColor(taskList.firstIndex(where: { $0.id == question.id }) == currentViewOrder ? Color.blue : Color.primary)
-                                    
-                                    Spacer()
-                                    
-                                    Text(question.task)
-                                        .font(.caption)
-                                    
-                                    Spacer()
-                                    
-                                    // Tick Mark (Dynamic)
-                                    if answer.answerRecord.count > taskList.firstIndex(where: { $0.id == question.id })! && answer.answerRecord[taskList.firstIndex(where: { $0.id == question.id })!] != "Not Answered" {
-                                        Image(systemName: "checkmark.circle.fill")
-                                            .foregroundColor(.green)
-                                    }
-                                }
-                            }
-                        }
-                        .frame(height: isQuestionListExpanded ? 200 : 0)
-                        .clipped()
-                    }
-                    .padding()
                 }
-                .navigationBarTitle("Examination", displayMode: .inline)
-                .alert(isPresented: $showAlert) {
+                .padding()
+                DisclosureGroup("Jump to Question", isExpanded: $isQuestionListExpanded) {
+                    List(taskList) { question in
+                        Button(action: {
+                            if let index = taskList.firstIndex(where: { $0.id == question.id }) {
+                                currentViewOrder = index
+                                isQuestionListExpanded = false
+                            }
+                        }) {
+                            Text(question.task)
+                        }
+                    }
+                    .frame(height: isQuestionListExpanded ? 200 : 0)
+                    .clipped()
+                }
+                .padding()
+            }
+            // when Vstak shows
+            .navigationBarTitle("Examination", displayMode: .inline)
+            .alert(isPresented: $showAlert) {
+                if taskList[currentViewOrder].task == "Monofilament test" || taskList[currentViewOrder].task == "IPSWICH touch test"
+                {
+                    Alert(
+                        title: Text("Not completed!"),
+                        message: Text("Please, enter the answer and make sure save the answer with the yellow button."),
+                        dismissButton: .default(Text("OK"))
+                    )
+                }
+                else {
                     Alert(
                         title: Text("Not completed!"),
                         message: Text("Please, enter the answer."),
                         dismissButton: .default(Text("OK"))
-                    )
-                }
+                    )}
             }
         }
         
     }
-        func checkMedicalTool() -> Bool
+    
+    func allTaskDone()
+    {
+        if (answer.checkCompletion())
         {
-            if answer.haveMedicalTool == "yes" {
-                return true
-            }
-            else {
-                return false
-            }
+            assessmentTaskDone = true
         }
-        func checkQuestionCompletion_PT_test()
-        {
-            if answer.answerRecord[14] == "Not Answered" || answer.answerRecord[15] == "Not Answered"
-            {
+    }
+    
+    func checkMedicalTool() -> Bool
+    {
+        if answer.haveMedicalTool == "yes" {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    func checkQuestionCompletion_DP_test() {
+       if answer.answerRecord[12] == "Not Answered" || answer.answerRecord[13] == "Not Answered" {
+         showAlert = true
+           isAnsweredDP = false
+       } else {
+           isAnsweredDP = true
+       }
+     }
+    func checkQuestionCompletion_PT_test() {
+        if answer.answerRecord[14] == "Not Answered" || answer.answerRecord[15] == "Not Answered" {
+            DispatchQueue.main.async {
                 showAlert = true
+                isAnsweredPT = false
             }
-            
-            if answer.answerRecord[14] != "Not Answered" || answer.answerRecord[15] != "Not Answered"
-            {
-                showAlert = false
-                isAnswered = true
-            }
+        } else {
+            isAnsweredPT = true
         }
-        func checkQuestionCompletion_DP_test()
-        {
-            if answer.answerRecord[12] == "Not Answered" || answer.answerRecord[13] == "Not Answered"
-            {
+    }
+    
+    func checkQuestionCompletion_MonoTest() {
+        if answer.answerRecord[16] == "Not Answered"{
+            DispatchQueue.main.async {
                 showAlert = true
+                isAnsweredMono = false
             }
-            
-            if answer.answerRecord[12] != "Not Answered" || answer.answerRecord[13] != "Not Answered"
-            {
-                showAlert = false
-                isAnswered = true
-            }
+        } else {
+            isAnsweredMono = true
         }
-        
+    }
+    
+    func checkQuestionCompletion_SWTCH() {
+        if answer.answerRecord[17] == "Not Answered"{
+            DispatchQueue.main.async {
+                showAlert = true
+                isAnsweredSwitch = false
+            }
+        } else {
+            isAnsweredSwitch = true
+        }
+    }
+    
     
     
 }
-    
